2a408253a5802bbafa4311ce3eaa1cecad82ce39
Ben Dean <bddbbd.b@gmail.com>
2020-02-06 19:11:25 -0800

    feat: basic data model

--- shows/models.py
+++ shows/models.py
@@ -1,32 +1,57 @@
+import uuid
 from django.db import models
 from django.template.defaultfilters import slugify
 
-class AutoSlugModelMixin(models.Model):
+class MediaBase(models.Model):
     """
-    Mixin to automatically create a slug given a title
+    Abstract base class for all media types having a title,
+    and thus a slug, complemented by a UUID
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    slug = models.SlugField(max_length=256, unique=True, null=False, blank=False, default='')
+    title = models.CharField(max_length=256, unique=True, null=False, blank=False)
+
+    class Meta:
+        abstract = True
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title) if self.slug is None or self.slug == '' else self.slug
         return super(__class__, self).save(*args, **kwargs)
 
 
+class Channel(MediaBase):
+    """
+    A media source, e.g. Hulu, Netflix, etc.
+    """
 
-class Show(AutoSlugModelMixin):
+
+class Show(MediaBase):
     """
     A 'Television' show, the top-level grouping of all the
     series/seasons/episodes
     """
-    title = models.CharField(max_length=256, unique=True, null=False, blank=False)
-    slug = models.SlugField(max_length=256, unique=True, null=False, blank=False)
 
-class Series(AutoSlugModelMixin):
+
+class Series(MediaBase):
     """
     What the American viewing audience might call a Season, represents a grouping of Episodes within a show
     """
-    pass
+    show = models.ForeignKey(Show, related_name="shows", on_delete=models.CASCADE)
+    number = models.IntegerField(default=0)
+    channels = models.ManyToManyField(Channel)
 
-class Episode(AutoSlugModelMixin):
+    class Meta:
+        unique_together = [['show', 'number']]
+
+class Episode(MediaBase):
     """
     The actual thing you can watch, and its metadata
     """
-    pass
+    series = models.ForeignKey(Series, related_name="series", on_delete=models.CASCADE)
+    number = models.IntegerField(default=0)
+    video = models.URLField(null=False, blank=False)
+    air_date = models.DateTimeField()
+
+    class Meta:
+        unique_together = [['series', 'number'],]
+
--- shows/tests/test_models.py
+++ shows/tests/test_models.py
@@ -9,9 +9,14 @@ class AutoSlugMixinTestCase(TestCase):
         self.assertEqual(howdy_doody.slug, "howdy-doodys-fun-time-hour")
 
     def test_no_slug_overwrite_on_save(self):
-        howdy_doody = Show.objects.create(title="Love Boat")
-        self.assertEqual(howdy_doody.slug, 'love-boat')
-        howdy_doody.title = 'Love Vessel'
-        self.assertEqual(howdy_doody.slug, 'love-boat')
+        show = Show.objects.create(title="Love Boat")
+        self.assertEqual(show.slug, 'love-boat')
+        show.title = 'Love Vessel'
+        self.assertEqual(show.slug, 'love-boat')
+
+    def test_uuid_mixin(self):
+        my_show = Show.objects.create(title="Twighlight Zone")
+        self.assertIsNotNone(my_show.id)
+        self.assertEqual(len(my_show.id.bytes), 16)
 
 
